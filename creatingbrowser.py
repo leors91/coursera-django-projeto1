1cef07800eaa17efd0f627acfcbfff420f9502e6
Leonardo Rodrigues <leorsantos657@gmail.com>
2025-01-27 21:07:49 -0300

    initial

---
+++ creatingbrowser.py
@@ -0,0 +1,19 @@
+import socket 
+
+""" 
+Entendendo como se dá o proceso de comunição enntre servidores e clentes com protocolo HTTP 
+
+"""
+
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(('data.pr4e.org',80))
+cmd = 'GET http://data.pr4e.org/page1.htm HTTP/1.0\r\n\r\n'.encode()
+mysock.send(cmd)
+
+while True:
+    data = mysock.recv(512)
+    if len(data) < 1:
+        break
+    print(data.decode(), end='')
+
+mysock.close()
